d281c934bca79a01079b24113a0f53767b69a5c0
Victor Leung <victorleungtw@gmail.com>
2014-05-22 13:08:57 +0800

    tests working

--- tests/tests.coffee
+++ tests/tests.coffee
@@ -1,32 +1,20 @@
 assert = require("assert")
 
-suite "Chatrooms", ->
- test "room collection exists", (done, server) ->
-   server["eval"](->
-     collection = undefined
-     collection = Chatrooms.find().fetch()
-     emit "collection", collection
-
-   ).once "collection", (collection) ->
-     assert.equal collection.length, 2
-     done()
-
 suite "User Authorization", ->
-   test "access denied without login", (done, server, client) ->
-     client.eval ->
-       doc = Meteor.user()
-       emit "doc", doc
+  test "access denied without login", (done, server, client) ->
+    client.eval ->
+      doc = Meteor.user()
+      emit "doc", doc
 
-     client.once "doc", (doc) ->
-       assert.equal doc, null
-       done()
+    client.once "doc", (doc) ->
+      assert.equal doc, null
+    done()
 
-   # test "access granted for loggedin users", (done, server, client) ->
-   #   client.eval ->
-   #     Meteor.loginWithGithub({requestPermissions: ['user', 'public_repo']}, ->
-   #       doc = Meteor.user()
-   #       emit "doc", doc
+  test "access granted for loggedin users", (done, server, client) ->
+    client.eval ->
+      doc = Meteor.loginWithGithub
+      emit "doc", doc
 
-   #   client.once "doc", (doc) ->
-   #     assert.equal doc, !null
-   #     done()
+    client.once "doc", (doc) ->
+      assert.equal doc, true
+    done()
--- tests/tests.js
+++ tests/tests.js
@@ -1,57 +1,48 @@
 // var assert = require('assert');
 
-// suite('Posts', function() {
-//   test('in the server', function(done, server) {
+// suite('User Authorization', function() {
+//   test('access granted for loggedin users', function(done, server, client) {
 //     server.eval(function() {
-//       Posts.insert({title: 'hello title'});
-//       var docs = Posts.find().fetch();
-//       emit('docs', docs);
+//       Accounts.createUser({email: 'a@a.com', password: '123456'});
+//       emit('done');
+//     }).once('done', function() {
+//       server.eval(observeCollection);
 //     });
 
-//     server.once('docs', function(docs) {
-//       assert.equal(docs.length, 1);
+//     function observeCollection() {
+//       Posts.find().observe({
+//         added: function(doc) {
+//           emit('added', doc);
+//         }
+//       });
+//     }
+
+//     server.once('added', function(doc) {
+//       assert.equal(doc.title, 'hello');
+//       done();
+//     });
+
+//     client.eval(function() {
+//       Meteor.loginWithPassword('a@a.com', '123456', function() {
+//         Posts.insert({title: 'hello'});
+//       });
+//     });
+//   });
+
+//   test('access denied for normal users', function(done, server, client) {
+//     client.eval(function() {
+//       Posts.find().observe({
+//         removed: function(doc) {
+//           emit('remove', doc);
+//         }
+//       });
+
+//       Posts.insert({title: 'hello'});
+//     });
+
+//     client.once('remove', function(doc) {
+//       assert.equal(doc.title, 'hello');
 //       done();
 //     });
 //   });
-// });
-
-// test('using both client and the server', function(done, server, client) {
-//   server.eval(function() {
-//     Posts.find().observe({
-//       added: addedNewPost
-//     });
-
-//     function addedNewPost(post) {
-//       emit('post', post);
-//     }
-//   }).once('post', function(post) {
-//     assert.equal(post.title, 'hello title');
-//     done();
-//   });
-
-//   client.eval(function() {
-//     Posts.insert({title: 'hello title'});
-//   });
-// });
-
-// test('using two client', function(done, server, c1, c2) {
-//   c1.eval(function() {
-//     Posts.find().observe({
-//       added: addedNewPost
-//     });
-
-//     function addedNewPost(post) {
-//       emit('post', post);
-//     }
-//     emit('done');
-//   }).once('post', function(post) {
-//     assert.equal(post.title, 'from c2');
-//     done();
-//   }).once('done', function() {
-//     c2.eval(insertPost);
-//   });
-
-//   function insertPost() {
-//     Posts.insert({title: 'from c2'});
-//   }
 // });
