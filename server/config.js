f5f70ee822166cbaad6dc6250aa4077bb112b4c1
Victor Leung <victorleungtw@gmail.com>
2014-05-22 14:48:55 +0800

    test

---
+++ server/config.js
@@ -0,0 +1,9 @@
+// Accounts.loginServiceConfiguration.remove({
+//   service: "github"
+// });
+
+// Accounts.loginServiceConfiguration.insert({
+//   service: "github",
+//   clientId: "5f66f33a3a01d434ffee",
+//   secret: "4945ecad115f1b858078c395483f77f369fc355c"
+// })
